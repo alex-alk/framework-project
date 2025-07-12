60f8ac237a7d988c42dbb1f51cda754beaa9d7f2
alex-alk <alex.leo06@yahoo.ro>
2025-07-12 17:49:33 +0300

    first commit

--- Controllers/WebController.php
+++ Controllers/WebController.php
@@ -8,10 +8,14 @@ use Router\View;
 
 class WebController
 {
+
+    public function __construct(private Logger $logger)
+    {
+    }
+
     public function index(): View
     {
-        $logger = new Logger();
-        $logger->debug('abc');
+        $this->logger->debug('abc');
         $users = User::all();
         print_r($users);
         return View::make('home');
